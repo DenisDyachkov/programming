1f73d47e1e6b3eab379c6c350d8f7ffdc2051909
Denis <kimak911@mail.ru>
2022-10-20 15:43:14 +0300

    сделал задание 2-2

---
+++ Task 2/task2-2/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+/**
+ * \ brief Функция расчета для аргумента меньше, чем пороговое значение
+ * \ param x - Аргумент функции
+ * \ param a- Константа функции
+ * \ return Значение функции в точке \a x.
+ */
+
+double getFunction1(const double x);
+
+/**
+* \ brief Функция расчета для аргумента больше или равно пороговому значению
+* \ param x - Аргумент функции
+* \ param a- Константа функции
+* \ return Значение функции в точке \a x.
+*/
+double getFunction2(const double x, const double a);
+
+/**
+ * \ brief точка входа в программу
+ * \ return возвращает 0 в случае успеха
+ */
+int main()
+{
+    cout << "Введите значение x = ";
+    double x = 0.0;
+    cin >> x;
+
+    const double THRESHOLD = 1;
+    const double a = 3.5;
+
+    if (x < THRESHOLD)
+    {
+        const double y = getFunction1(x);
+        cout << " y = " << y;
+    }
+    else
+    {
+        const double y = getFunction2(x, a);
+        cout << " y = " << y;
+    }
+
+    return 0;
+}
+
+double getFunction1(x)
+{
+    return log10(x + 1)
+}
+
+double getFunction2(x, a)
+{
+    return sin(2) * sqrt(| a * x | )
+}
