a58c1f0ed18a60f97365fbf02c3f563081726962
DenisDyachkov <113631170+DenisDyachkov@users.noreply.github.com>
2023-01-16 11:36:19 +0300

    Update main.cpp

--- Task 2/Task 2-1/main.cpp
+++ Task 2/Task 2-1/main.cpp
@@ -1,5 +1,6 @@
-﻿#include <iostream>
-
+#define USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 /**
@@ -8,196 +9,126 @@ using namespace std;
 * \return Значение переменной.
 */
 
-double getNumber(const string& message);
+double getvariable(const string& message);
+
+/**
+*\brief Функция, суммирующая два числа.
+*\param x Первое число.
+*\param y Второе число.
+*\return Значение функции.
+*/
+
+double getSum(const double x, const double y);
+
+/**
+*\brief Функция вычитающая одно число из другого.
+*\param x Первое число.
+*\param y Второе число.
+*/
+
+double difference(const double x, const double y);
+
+/**
+*\brief Функция, умножающая одно число на другое.
+*\param a Первое число.
+*\param b Второе число.
+*/
+
+double composition(const double a, const double b);
+
+/**
+*\brief Функция делящая одно число на другое.
+*\param x Первое число.
+*\param y Второе число.
+*/
+
+double quotient(const double x, const double y);
+
+/**
+*\brief Точка входа в программу.
+*\return Возвращает 0 в случае успеха.
+*/
 
 enum class userInput
 {
-	DAY_OF_THE_WEEK,
-	MONTH
+sum,
+difference,
+composition,
+quotient
+
 };
-
-/**
-* \brief Считывает порядковый номер дня недели.
-* \param number Порядковый номер дня недели.
-* \return Название дня недели.
-*/
-
-string getDay(const int number);
-
-/**
-* \brief Считывает порядковый номер месяца.
-* \param number Порядковый номер месяца.
-* \return Название месяца.
-*/
-
-string getMonth(const int number);
-
-/*
-*\brief Точка входа в программу
-*\return возвращает 0 в случае успеха
-*/
 
 int main()
 {
-	cout « "Введите число, соответствующее заданию:\n"
-		« static_cast<int>(userInput::DAY_OF_THE_WEEK) « " - Определение названия дня недели по номеру\n"
-		« static_cast<int>(userInput::MONTH) « " - Определение названия месяца по номеру\n";
+double x = getvariable("Введите значение перменной x: ");
+double y = getvariable("Введите значение перменной y: ");
+cout « "Введите число, соответствующее заданию:\n"
+« static_cast<int>(userInput::sum) « " - Вычисляет сумму чисел x и y.\n"
+« static_cast<int>(userInput::difference) « " - Вычисляет разность чисел x и y.\n"
+« static_cast<int>(userInput::composition) « " - Вычисляет произведение чисел x и y.\n"
+« static_cast<int>(userInput::quotient) « " - Вычисляет частное чисел x и y.\n";
 
-	int input = 0;
-	cin »  input;
-	const auto choice = static_cast<userInput>(input);
+int input = 0;
+cin » input;
+const auto choice = static_cast<userInput>(input);
+cout « "\n";
 
-	switch (choice)
-	{
-	case userInput::DAY_OF_THE_WEEK:
-	{
-		const double number = getNumber("Введите номер дня: ");
-		cout « number « " день недели" « " - " « getDay(number) « "\n";
-		break;
-	}
-
-	case userInput::MONTH:
-	{
-		const double number = getNumber("Введите номер месяца: ");
-		cout « number « " месяц" « " - " « getMonth(number) « "\n";
-		break;
-	}
-	}
-
-	return 0;
+switch(choice)
+{
+case userInput::sum:
+{
+const double t = getSum(x, y);
+cout « "Сумма равна = " « t « endl;
+break;
 }
 
-double getNumber(const string& message)
+case userInput::difference:
 {
-	cout « message;
-	int variable = 0;
-	cin » variable;
-	return variable;
+const double t = difference(a, b);
+cout « "Разность x - y = " « t « endl;
+cout « "Разность y - x = " « (-1)*t « endl;
+break;
+}
+case userInput::composition:
+{
+const double t = composition(x, y);
+cout « "Произведение x и y = " « t « endl;
+break;
+}
+case userInput::quotient:
+{
+const double t = quotient(x, y);
+cout « "Частное x и y = " « t « endl;
+break;
+}
+}
+return 0;
 }
 
-string getDay(const int number)
+double getvariable(const string& message)
 {
-	switch (number)
-	{
-	case 1:
-	{
-		return "понедельник";
-		break;
-	}
-
-	case 2:
-	{
-		return "вторник";
-		break;
-	}
-
-	case 3:
-	{
-		return "среда";
-		break;
-	}
-
-	case 4:
-	{
-		return "четверг";
-		break;
-	}
-
-	case 5:
-	{
-		return "пятница";
-		break;
-	}
-
-	case 6:
-	{
-		return "суббота";
-		break;
-	}
-
-	case 7:
-	{
-		return "воскресенье";
-		break;
-	}
-	}
+cout « message;
+double variable = 0.0;
+cin » variable;
+return variable;
 }
 
-string getMonth(const int number)
+double getSum(const double x, const double y)
 {
-	switch (number)
-	{
-	case 1:
-	{
-		return "январь";
-		break;
-	}
+return x + y;
+}
 
-	case 2:
-	{
-		return "февраль";
-		break;
-	}
+double difference(const double x, const double y)
+{
+return x - y;
+}
 
-	case 3:
-	{
-		return "март";
-		break;
-	}
+double composition(const double x, const double y)
+{
+return x * y;
+}
 
-	case 4:
-	{
-		return "апрель";
-		break;
-	}
-
-	case 5:
-	{
-		return "май";
-		break;
-	}
-
-	case 6:
-	{
-		return "июнь";
-		break;
-	}
-
-	case 7:
-	{
-		return "июль";
-		break;
-	}
-
-	case 8:
-	{
-		return "август";
-		break;
-	}
-
-	case 9:
-	{
-		return "сентябрь";
-		break;
-	}
-
-	case 10:
-	{
-		return "октябрь";
-		break;
-	}
-
-	case 11:
-	{
-		return "ноябрь";
-		break;
-	}
-
-	case 12:
-	{
-		return "декабрь";
-		break;
-	}
-
-	}
+double quotient(const double x, const double y)
+{
+return x / y;
 }
