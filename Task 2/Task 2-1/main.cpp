177bc9c3b62c0ef96229bcffaa7f12e76df32e8d
DenisDyachkov <113631170+DenisDyachkov@users.noreply.github.com>
2023-01-21 01:10:31 +0300

    Update main.cpp

--- Task 2/Task 2-1/main.cpp
+++ Task 2/Task 2-1/main.cpp
@@ -1,134 +1,111 @@
-#define USE_MATH_DEFINES
-#include <iostream>
-#include <cmath>
-using namespace std;
+это вариант 2 
+делал сам так как этот вариант никто из группы не делал
+делал по вашим видео
 
+#define _USE_MATH_DEFINES
+ #include <iostream>
+ #include <cmath>
+ using namespace std;
+ 
 /**
-* \brief Считывает переменную из консоли.
-* \param message Побуждающее сообщение для пользователя.
-* \return Значение переменной.
-*/
-
-double getvariable(const string& message);
-
+ * \brief Рассчитывает периметр треугольника.
+ * \param first первый катет прямоугольного треугольника.
+ * \param second второй катет прямоугольного треугольника.
+ * \return периметр треугольника.
+ */
+ double getPerimetr(double first, double second);
+ 
 /**
-*\brief Функция, суммирующая два числа.
-*\param x Первое число.
-*\param y Второе число.
-*\return Значение функции.
-*/
-
-double getSum(const double x, const double y);
-
+ * \brief Рассчитывает площадь треугольника.
+ * \param first первый катет прямоугольного треугольника.
+ * \param second второй катет прямоугольного треугольника.
+ * \return площадь треугольника.
+ */
+ double getArea(double first, double second);
+ 
+ /**
+ * \brief считать сторону трегольника из консоли.
+ * \param message побуждающая надпись для пользователя.
+ * \return сторона треугольника треугольника.
+ */
+ double getSide( const string& message = "");
+ 
 /**
-*\brief Функция вычитающая одно число из другого.
-*\param x Первое число.
-*\param y Второе число.
-*/
-
-double difference(const double x, const double y);
-
+ * \brief пользовательский ввод
+ * \param PERIMETR расчет периметра треугольника
+ * \param AREA расчет площади треугольника
+ */
+ enum class userInput
+ {
+     PERIMETR = 1,
+     AREA
+ };
+ 
 /**
-*\brief Функция, умножающая одно число на другое.
-*\param a Первое число.
-*\param b Второе число.
-*/
-
-double composition(const double a, const double b);
-
-/**
-*\brief Функция делящая одно число на другое.
-*\param x Первое число.
-*\param y Второе число.
-*/
-
-double quotient(const double x, const double y);
-
-/**
-*\brief Точка входа в программу.
-*\return Возвращает 0 в случае успеха.
-*/
-
-enum class userInput
-{
-sum,
-difference,
-composition,
-quotient
-
-};
-
-int main()
-{
-double x = getvariable("Введите значение перменной x: ");
-double y = getvariable("Введите значение перменной y: ");
-cout « "Введите число, соответствующее заданию:\n"
-« static_cast<int>(userInput::sum) « " - Вычисляет сумму чисел x и y.\n"
-« static_cast<int>(userInput::difference) « " - Вычисляет разность чисел x и y.\n"
-« static_cast<int>(userInput::composition) « " - Вычисляет произведение чисел x и y.\n"
-« static_cast<int>(userInput::quotient) « " - Вычисляет частное чисел x и y.\n";
-
-int input = 0;
-cin » input;
-const auto choice = static_cast<userInput>(input);
-cout « "\n";
-
-switch(choice)
-{
-case userInput::sum:
-{
-const double t = getSum(x, y);
-cout « "Сумма равна = " « t « endl;
-break;
-}
-
-case userInput::difference:
-{
-const double t = difference(a, b);
-cout « "Разность x - y = " « t « endl;
-cout « "Разность y - x = " « (-1)*t « endl;
-break;
-}
-case userInput::composition:
-{
-const double t = composition(x, y);
-cout « "Произведение x и y = " « t « endl;
-break;
-}
-case userInput::quotient:
-{
-const double t = quotient(x, y);
-cout « "Частное x и y = " « t « endl;
-break;
-}
-}
-return 0;
-}
-
-double getvariable(const string& message)
-{
-cout « message;
-double variable = 0.0;
-cin » variable;
-return variable;
-}
-
-double getSum(const double x, const double y)
-{
-return x + y;
-}
-
-double difference(const double x, const double y)
-{
-return x - y;
-}
-
-double composition(const double x, const double y)
-{
-return x * y;
-}
-
-double quotient(const double x, const double y)
-{
-return x / y;
-}
+ * \brief
+ * \return
+ */
+ int main()
+ {
+    try 
+    {
+     const double first = getSide("Введите значение первого катета треугольника = ");
+     const double second = getSide("Введите значение второго катета треугольника = ");
+     
+     
+     cout << "Введите число,соответствующее заданию.\n"
+     << static_cast<int>(userInput::AREA) << "- вычесление площади треугольника.\n "
+     << static_cast<int>(userInput::PERIMETR) << "- вычесление периметра треугольника.\n "
+     
+     int input = 0;
+     cin >> input;
+     const auto choice = static_cast<userInput>(input);
+     
+     switch(choice)
+     {
+     case userInput::AREA:
+         {
+             const auto area = getArea(first,second);
+             cout << "Площадь треугольника = " << area;
+             break;
+         }
+     case userInput::PERIMETR:
+         {
+             const auto perimetr = getPerimetr(first,second);
+             cout << "Периметр треугольника = " << perimetr;
+             break;
+         }
+     }
+     
+    }
+     catch (out_of_range&)
+     {
+         cout << "Введена неправильная сторона!";
+         return 1;
+     }
+     return 0;
+ }
+ 
+ double getPerimetr(const double first, const double second)
+ {
+     return first + second + sqrt(pow(first,2) + pow(second,2))
+ }
+ 
+ double getArea(const double first, const double second)
+ {
+     return (first * second)/2
+ }
+ 
+ double getSide(const string& message)
+ {
+     cout << message;
+     double side = 0.0;
+     cin >> side;
+     if (side < = 0)
+     {
+         throw out_of_range("Введена неправильная сторона!");
+     }
+     
+     return side;
+ }
+ 
